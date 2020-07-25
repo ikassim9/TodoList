7bbcfacbe29a2365a7f5b845e686199f6b7dec4d
Ismail Kassim <jq3583el@go.minnstate.edu>
2020-07-24 21:33:05 -0500

    add alarm manager for notification

--- app/src/main/java/com/ismail/todolist/MainActivity.kt
+++ app/src/main/java/com/ismail/todolist/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.NotificationCompat
+import androidx.fragment.app.replace
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupActionBarWithNavController
 
@@ -12,9 +13,17 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setupActionBarWithNavController(findNavController(R.id.fragment))
+        val extra = intent.extras
+        if(extra != null){
+            if(extra.containsKey("key")){
+                val fragmentTrasaction =supportFragmentManager.beginTransaction()
+                fragmentTrasaction.replace(R.id.fragment, DetailFragment()
+                )
 
+            }
         }
 
+        }
 
     override fun onSupportNavigateUp(): Boolean {
         val navController = findNavController(R.id.fragment)
@@ -22,8 +31,3 @@ class MainActivity : AppCompatActivity() {
         return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
-
-
-//    fun sendNotification(){
-//    val notifcation = NotificationCompat.Builder(@MainActivity, NotificationHelper.reminderChannel)
-//}
