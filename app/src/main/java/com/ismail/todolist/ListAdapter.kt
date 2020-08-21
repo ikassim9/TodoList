462e8222eceef3a239cb50b664d0b71344f07037
Ismail Kassim <jq3583el@go.minnstate.edu>
2020-08-20 21:13:24 -0500

    add search functionality

--- app/src/main/java/com/ismail/todolist/ListAdapter.kt
+++ app/src/main/java/com/ismail/todolist/ListAdapter.kt
@@ -3,21 +3,26 @@ package com.ismail.todolist
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Filter
+import android.widget.Filterable
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.ismail.todolist.db.TodoItem
-import kotlinx.android.synthetic.main.fragment_detail.view.*
 import kotlinx.android.synthetic.main.item_list.view.*
+import java.util.*
+import kotlin.collections.ArrayList
 
 
 class ListAdapter(
     private val callback: AdapterCallBack
 
 ) :
-    RecyclerView.Adapter<ListAdapter.MyViewHolder>() {
+    RecyclerView.Adapter<ListAdapter.MyViewHolder>(), Filterable {
 
-    private var todoList: List<TodoItem> = ArrayList()
-    var selected: Boolean = false
+    private var todoList: MutableList<TodoItem> = ArrayList()
+
+    // contain user searches
+    lateinit var searchTodoList: List<TodoItem>
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
         val context = parent.context
@@ -41,19 +46,22 @@ class ListAdapter(
         val diffResult: DiffUtil.DiffResult = DiffUtil.calculateDiff(
             TodoItemDiffCal(oldList, todoItem)
         )
-        todoList = todoItem
+        todoList = todoItem as MutableList<TodoItem>
         diffResult.dispatchUpdatesTo(this)
 
     }
 
     inner class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        init {
+            searchTodoList = ArrayList(todoList)
+        }
+
         fun bind(todoItem: TodoItem) {
             val position = adapterPosition
             itemView.doneCheckBox.isChecked = false
             itemView.tvTitle.text = todoItem.title
             itemView.dueDate.text = todoItem.dueDate
             itemView.doneCheckBox.isChecked = false
-        //    itemView.btnNotificationStatus.isChecked = todoItem.notificationOnOrOff
             itemView.setOnClickListener() {
                 callback.onItemClick(todoItem, position)
             }
@@ -65,12 +73,12 @@ class ListAdapter(
                     position
                 )
             }
-            itemView.setOnLongClickListener{
+            itemView.setOnLongClickListener {
                 callback.onItemLongClick(todoItem, position)
             }
         }
     }
-
+// Compares two list and returns optimize result
     class TodoItemDiffCal(
         private var oldList: List<TodoItem>,
         private var newList: List<TodoItem>
@@ -92,9 +100,34 @@ class ListAdapter(
             return oldList[oldItemPosition] == newList[newItemPosition]
         }
     }
-    fun addItem(position: Int): TodoItem {
-        return todoList[position]
+
+    // Filter search results
+    override fun getFilter(): Filter {
+        return object : Filter() {
+            override fun performFiltering(constraint: CharSequence?): FilterResults {
+                val filterList: MutableList<TodoItem> = ArrayList()
+                if (constraint == null || constraint.isEmpty()) {
+                    filterList.addAll(searchTodoList)
+                } else {
+                    val filterPattern: String = constraint.toString().toLowerCase(Locale.ROOT)
+                        .trim()
+                    for (item: TodoItem in searchTodoList) {
+                        if (item.title.toLowerCase(Locale.ROOT).startsWith(filterPattern)) {
+                            filterList.add(item)
+                        }
+                    }
+                }
+                val searchResult = FilterResults()
+                searchResult.values = filterList
+                return searchResult
+            }
+
+            override fun publishResults(constraint: CharSequence?, results: FilterResults?) {
+                todoList.clear()
+                todoList.addAll((results?.values) as List<TodoItem>)
+                notifyDataSetChanged()
+
+            }
+        }
     }
-
 }
-
--- app/src/main/java/com/ismail/todolist/MainFragment.kt
+++ app/src/main/java/com/ismail/todolist/MainFragment.kt
@@ -9,8 +9,11 @@ import android.graphics.Color
 import android.os.Bundle
 import android.util.Log
 import android.view.*
+import android.view.inputmethod.EditorInfo
 import android.widget.Toast
+//import androidx.appcompat.widget.SearchView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.SearchView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
@@ -45,17 +48,23 @@ class MainFragment : Fragment(), AdapterCallBack {
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
-        inflater.inflate(R.menu.delete_all, menu)
+        inflater.inflate(R.menu.menu_bar, menu)
+        setUpSearchFilter(menu)
 
         super.onCreateOptionsMenu(menu, inflater)
+//        java.lang.ClassCastException: androidx.appcompat.widget.SearchView cannot be cast to android.widget.SearchView
+
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
             R.id.delete_all_items -> {
-                deleteAllItemDiolag()
-                cancelAlarm()
+                deleteAllItemDialog()
+                cancelAllPendingIntent()
                 return true
+            }
+            R.id.search_filter -> {
+
             }
         }
         return super.onOptionsItemSelected(item)
@@ -67,9 +76,10 @@ class MainFragment : Fragment(), AdapterCallBack {
         setHasOptionsMenu(true)
     }
 
-    override fun checkBoxListener(view: View, ischeck: Boolean, todoItem: TodoItem, position: Int) {
-        if (ischeck) {
+    override fun checkBoxListener(view: View, isCheck: Boolean, todoItem: TodoItem, position: Int) {
+        if (isCheck) {
             todoViewModel.deleteItem(todoItem)
+            cancelSpecificPendingIntent(todoItem.id)
             undoCompletedTaskSnackbar(todoItem)
             Log.i("delete_check", "$todoItem is check at position $position")
 
@@ -152,7 +162,7 @@ class MainFragment : Fragment(), AdapterCallBack {
         snackbar.show()
     }
 
-    private fun cancelAlarm() {
+    private fun cancelAllPendingIntent() {
         val alarmManager = requireActivity().getSystemService(Context.ALARM_SERVICE) as AlarmManager
         val intent = Intent(requireContext(), NotificationReceiver::class.java)
         val pendingIntent = PendingIntent.getBroadcast(
@@ -160,23 +170,59 @@ class MainFragment : Fragment(), AdapterCallBack {
             1, intent, 0
         )
         alarmManager.cancel(pendingIntent)
-        Log.d("alarm_cancel", "alarm canceled!")
+        Log.d("pending_cancel", "pendingIntent canceled!")
     }
 
-    private fun deleteAllItemDiolag() : Boolean {
+    private fun deleteAllItemDialog(): Boolean {
         val dialogBuilder = AlertDialog.Builder(requireContext())
         dialogBuilder.setTitle("Delete all tasks?")
         dialogBuilder.setMessage("Do you want to delete all tasks?")
             .setCancelable(false)
             .setPositiveButton("Yes") { dialog, id ->
                 todoViewModel.deleteAllItems()
-                Toast.makeText(requireContext(), "All tasks have beeng deleted", Toast.LENGTH_SHORT).show()
+                Toast.makeText(requireContext(), "All tasks have beeng deleted", Toast.LENGTH_SHORT)
+                    .show()
 
             }
             .setNegativeButton("Cancel") { dialog, id ->
                 dialog.cancel()
             }.show()
-    return true
+        return true
 
-}
+    }
+// Allows user to filter search the recyclerview and returns data that starts with that input characters
+    private fun setUpSearchFilter(menu: Menu): Boolean {
+        val searchItem = menu.findItem(R.id.search_filter)
+        if (searchItem != null) {
+            val searchView: SearchView = searchItem.actionView as SearchView
+            searchView.queryHint = "Search your tasks"
+            searchView.imeOptions = EditorInfo.IME_ACTION_DONE
+            searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+                override fun onQueryTextSubmit(query: String?): Boolean {
+                    return false
+                }
+
+                override fun onQueryTextChange(newText: String?): Boolean {
+                    adapter.filter.filter(newText)
+                    return false
+
+                }
+
+            })
+
+        }
+        return true
+
+
+    }
+
+    private fun cancelSpecificPendingIntent(id: Int) {
+        val alarmManager = requireActivity().getSystemService(Context.ALARM_SERVICE) as AlarmManager
+        val intent = Intent(requireContext(), NotificationReceiver::class.java)
+        val pendingIntent = PendingIntent.getBroadcast(
+            requireContext(),
+            4, intent, 0
+        )
+        Log.d("specific_pendingIntent_cancel", "Specific PendingIntent canceled!")
+    }
 }
