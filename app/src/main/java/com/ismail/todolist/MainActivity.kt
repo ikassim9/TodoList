9a4852f2a77b1720c92a501bdaa3b5bd7e9a8368
Ismail Kassim <jq3583el@go.minnstate.edu>
2020-08-20 20:13:27 -0500

    Add function which hides virtual keyboard on arrow backpress

--- app/src/main/java/com/ismail/todolist/MainActivity.kt
+++ app/src/main/java/com/ismail/todolist/MainActivity.kt
@@ -1,27 +1,39 @@
 package com.ismail.todolist
 
-import android.content.Context
-import android.content.Intent
-import android.hardware.input.InputManager
+import android.app.Activity
 import android.os.Bundle
+import android.view.inputmethod.InputMethodManager
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.app.NotificationCompat
-import androidx.fragment.app.replace
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupActionBarWithNavController
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setupActionBarWithNavController(findNavController(R.id.fragment))
-        val extra = intent.extras
+
     }
 
 
+    // controls backPress arrow
     override fun onSupportNavigateUp(): Boolean {
         val navController = findNavController(R.id.fragment)
+         hideVirtualKeyboardOnBackPress()
         return navController.navigateUp() || super.onSupportNavigateUp()
+
     }
 
+    // Hides  keyboard when back  button is pressed
+    private fun hideVirtualKeyboardOnBackPress() {
+        try {
+            val inputMethodManager =
+                this.getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
+            inputMethodManager.hideSoftInputFromWindow(this.currentFocus!!.windowToken, 0)
+
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
 }
